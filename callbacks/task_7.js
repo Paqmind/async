0820e83e75b6dbeedaad2072fbeee282ed2effde
Uladzislau97 <VSvasilyev97@gmail.com>
2018-02-02 16:45:43 +0300

    fix task 7

--- callbacks/task_7.js
+++ callbacks/task_7.js
@@ -1,15 +1,43 @@
 function parallel(tasks, callback) {
-  let results = []
+  let results
+
+  if (tasks instanceof Array) {
+    results = []
+  } else if (tasks instanceof Object) {
+    results = {}
+  } else {
+    tasks = {}
+    results = {}
+  }
+
   let keys = Object.keys(tasks)
+
   keys.forEach((key) => {
     tasks[key]((err, res) => {
       if (err) {
         callback(err)
       }
-      results.push(res)
-      if (callback && results.length === keys.length) {
-        callback(null, results)
+
+      results[key] = res
+
+      if (callback && Object.keys(results).length === keys.length) {
+        callback(err, results)
       }
     })
   })
 }
+
+parallel([
+    function(callback) {
+        setTimeout(function() {
+            callback(null, 1);
+        }, 1000);
+    },
+    function(callback) {
+        setTimeout(function() {
+            callback(null, 2);
+        }, 700);
+    }
+], function(err, results) {
+  console.log(results)
+});
