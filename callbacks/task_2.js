b9b9f7422116c037914d20272081374fc2bd6726
Uladzislau97 <VSvasilyev97@gmail.com>
2018-02-03 09:46:02 +0300

    tasks 2-3

---
+++ callbacks/task_2.js
@@ -0,0 +1,25 @@
+function afn1(next) {
+  next("a1")
+}
+
+function afn2(next) {
+  next("a2")
+}
+
+function afn3(next) {
+  next("a3")
+}
+
+function done(x1, x2, x3) {
+  console.log(x1, x2, x3)
+}
+
+afn1((x1) => {
+  afn2((x2) => {
+    afn3((x3) => {
+      done(x1, x2, x3)
+    })
+  })
+});
+
+// Expected to log "a1 a2 a3"
---
+++ callbacks/task_3.js
@@ -0,0 +1,31 @@
+function afn1(x, next) {
+  setTimeout(() => next("a1" + x), rand(0, 1000))
+}
+
+function afn2(x, next) {
+  setTimeout(() => next("a2" + x), rand(0, 1000))
+}
+
+function afn3(x, next) {
+  setTimeout(() => next("a3" + x), rand(0, 1000))
+}
+
+function done(rs) {
+  console.log(...rs)
+}
+
+function rand(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min
+}
+
+let afns = [afn1, afn2, afn3]
+
+let rs = []
+for (let i = 0; i < afns.length; i++) {
+  afns[i](i + 1, (r) => {
+    rs.push(r)
+    if (rs.length == afns.length) {
+      done(rs) // Expected to log "a11 a22 a33" in any order
+    }
+  })
+}
