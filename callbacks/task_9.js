459dc0dfd3e1094a16b440cf16c887d758d0c456
Uladzislau97 <VSvasilyev97@gmail.com>
2018-02-07 12:53:37 +0300

    task 9

--- callbacks/task_9.js
+++ callbacks/task_9.js
@@ -1,2 +1,25 @@
-module.exports = function waterfall(tasks, callback) {
+module.exports = function waterfall(fns, lastFn) {
+  let results = []
+
+  if (!lastFn) {
+    lastFn = function(err, res) {}
+  }
+
+  function go(params) {
+    let fn = fns.shift()
+    if (fn) {
+      fn(...params, function next(err) {
+        if (err) {
+          lastFn(err)
+        } else {
+          console.log(arguments)
+          results = [...arguments].slice(1)
+          go(results)
+        }
+      })
+    } else {
+      lastFn(null, ...results)
+    }
+  }
+  go([])
 }
