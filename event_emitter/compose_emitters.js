739fcafbcaaa7d5a511dc23863a78cfc5efaa510
Uladzislau97 <VSvasilyev97@gmail.com>
2018-02-13 16:51:38 +0300

    fix composing of event emitters

--- event_emitter/compose_emitters.js
+++ event_emitter/compose_emitters.js
@@ -1,57 +1,17 @@
 const EventEmitter = require('./event_emitter')
 
-class ComposeEventEmitter {
-  constructor (emitter) {
-    this.emitter = emitter
-    this.events = emitter.events
-  }
+let e1 = new EventEmitter()
+let e2 = new EventEmitter()
 
-  emit (event, arg) {
-    if ((this.events[event] || []).length) {
-      this.events[event].forEach((listener) => {
-        listener(arg)
-        listener(arg * 2)
-      })
-      return true
-    } else {
-      return false
-    }
-  }
-
-  addListener (event, listener) {
-    let listeners = this.events[event] || []
-    if (!listeners.includes(listener)) {
-      this.events[event] = listeners.concat([listener])
-    }
-    return this
-  }
-
-  removeListener (event, listener) {
-    this.events[event] = (this.events[event] || [])
-      .filter((cb) => {
-        cb != listener
-      })
-    return this
-  }
-
-  once (event, listener) {
-    let listenerWrapper = (arg) => {
-      listener(arg)
-      this.removeListener(event, listener)
-    }
-    this.events[event] = (this.events[event] || [])
-      .concat([listenerWrapper])
-    return listenerWrapper
-  }
-}
-
-let ee = new EventEmitter()
-let composeEmitter = new ComposeEventEmitter(ee)
-
-composeEmitter.addListener('data', (arg) => {
-  console.log(arg)
+e1.on("data", (x) => {
+  console.log("e1:", x)
+  e2.emit("data", x)
 })
 
-composeEmitter.emit('data', 1)
-composeEmitter.emit('data', 2)
-composeEmitter.emit('data', 3)
+e2.on("data", (x) => {
+  console.log("e2:", x * 2)
+})
+
+e1.emit("data", 1)
+e1.emit("data", 2)
+e1.emit("data", 3)
