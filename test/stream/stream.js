fd63fd65847f701e75f38d2a7b4b28ff0e7ca264
Uladzislau97 <VSvasilyev97@gmail.com>
2018-02-27 15:17:59 +0300

    fix stream tests

--- test/stream/stream.js
+++ test/stream/stream.js
@@ -1,158 +1,160 @@
 const assert = require('chai').assert
 const Stream = require('../../stream/stream')
 
-describe('event_emitter/event_emitter.js', function () {
-  describe('#observe()', function() {
-    it('should return unsubscribe function', function(done) {
-      let s$ = new Stream(({emit}) => {
-        let x = 0
-        let interval = setInterval(() => emit(x++), 10)
-        return () => clearInterval(interval)
-      })
-      let unsubscribeFn = s$.observe((arg) => 'result')
-      assert.isFunction(unsubscribeFn)
-      unsubscribeFn()
-      done()
-    })
+describe('stream/stream.js', () => {
+  describe('Stream', () => {
+    describe('#observe()', () => {
+      it('should call observeFn', (done) => {
+        let s$ = new Stream(({emit}) => {
+          let x = 0
+          let interval = setInterval(() => emit(x++), 10)
+          return () => clearInterval(interval)
+        })
+        let observeArgs = []
+        let unsubscribeFn = s$.observe((arg) => {
+          observeArgs.push(arg)
+        })
 
-    it('unsubscribe function should stop observing', function(done) {
-      let s$ = new Stream(({emit}) => {
-        let x = 0
-        let interval = setInterval(() => emit(x++), 10)
-        return () => clearInterval(interval)
+        setTimeout(() => {
+          unsubscribeFn()
+          assert.deepEqual(observeArgs, [0, 1, 2, 3])
+          done()
+        }, 45)
       })
-      let unsubscribeFn = s$.observe((arg) => 'result')
 
-      setTimeout(() => {
+      it('should call streamFn once', (done) => {
+        let streamFnCalls = 0
+        let s$ = new Stream(({emit}) => {
+          streamFnCalls++
+          let x = 0
+          let interval = setInterval(() => emit(x++), 10)
+          return () => clearInterval(interval)
+        })
+        let unsubscribeFn = s$.observe(_ => null)
+
+        setTimeout(() => {
+          unsubscribeFn()
+          assert(streamFnCalls == 1)
+          done()
+        }, 45)
+      })
+
+      it('should return unsubscribe function', (done) => {
+        let s$ = new Stream(({emit}) => {
+          let x = 0
+          let interval = setInterval(() => emit(x++), 10)
+          return () => clearInterval(interval)
+        })
+        let unsubscribeFn = s$.observe(_ => null)
+        assert.isFunction(unsubscribeFn)
         unsubscribeFn()
-        assert(Object.keys(s$.emitter.events).length == 0)
         done()
-      }, 45)
+      })
+
+      it('unsubscribe function should stop observing', (done) => {
+        let xs = []
+        let s$ = new Stream(({emit}) => {
+          let interval = setInterval(() => xs.push(true), 10)
+          return () => clearInterval(interval)
+        })
+        let unsubscribeFn = s$.observe(_ => null)
+
+        setTimeout(() => {
+          unsubscribeFn()
+          assert(xs.length == 2)
+          done()
+        }, 25)
+      })
     })
 
-    it('should call observeFn', function(done) {
-      let s$ = new Stream(({emit}) => {
-        let x = 0
-        let interval = setInterval(() => emit(x++), 10)
-        return () => clearInterval(interval)
-      })
-      let observeArgs = []
-      let unsubscribeFn = s$.observe((arg) => {
-        observeArgs.push(arg)
-      })
+    describe('#map()', () => {
+      it('should call observeFn with mapped args', (done) => {
+        let stream$ = new Stream( ({emit}) => {
+          let x = 0
+          let interval = setInterval(() => emit(x++), 10)
+          return () => clearInterval(interval)
+        })
+        let mappedStream$ = stream$.map((x) => x * 2)
+        let observeArgs = []
+        let unsubscribeFn = mappedStream$.observe((arg) => {
+          observeArgs.push(arg)
+        })
 
-      setTimeout(() => {
-        unsubscribeFn()
-        assert.deepEqual(observeArgs, [0, 1, 2, 3])
-        done()
-      }, 45)
+        setTimeout(() => {
+          unsubscribeFn()
+          assert.deepEqual(observeArgs, [0, 2, 4, 6])
+          done()
+        }, 45)
+      })
     })
 
-    it('should call streamFn once', function(done) {
-      let streamFnCalls = 0
-      let s$ = new Stream(({emit}) => {
-        streamFnCalls++
-        let x = 0
-        let interval = setInterval(() => emit(x++), 10)
-        return () => clearInterval(interval)
-      })
-      let unsubscribeFn = s$.observe((arg) => 'result')
+    describe('#filter()', () => {
+      it('should call observeFn with filtered args', (done) => {
+        let stream$ = new Stream( ({emit}) => {
+          let x = 0
+          let interval = setInterval(() => emit(x++), 10)
+          return () => clearInterval(interval)
+        })
+        let filteredStream$ = stream$.filter((x) => x % 2 == 0)
+        let observeArgs = []
+        let unsubscribeFn = filteredStream$.observe((arg) => {
+          observeArgs.push(arg)
+        })
 
-      setTimeout(() => {
-        unsubscribeFn()
-        assert(streamFnCalls == 1)
-        done()
-      }, 45)
+        setTimeout(() => {
+          unsubscribeFn()
+          assert.deepEqual(observeArgs, [0, 2])
+          done()
+        }, 45)
+      })
     })
-  })
 
-  describe('#map()', function() {
-    it('should call observeFn with mapped args', function(done) {
-      let stream$ = new Stream( ({emit}) => {
-        let x = 0
-        let interval = setInterval(() => emit(x++), 10)
-        return () => clearInterval(interval)
-      })
-      let mappedStream$ = stream$.map((x) => x * 2)
-      let observeArgs = []
-      let unsubscribeFn = mappedStream$.observe((arg) => {
-        observeArgs.push(arg)
-      })
+    describe('#scan()', () => {
+      it('should call observeFn with accumulator', (done) => {
+        let stream$ = new Stream(({emit}) => {
+          let x = 0
+          let interval = setInterval(() => emit(x++), 10)
+          return () => clearInterval(interval)
+        })
+        let scanedStream$ = stream$.scan(0, (x, y) => x + y)
+        let observeArgs = []
+        let unsubscribeFn = scanedStream$.observe((arg) => {
+          observeArgs.push(arg)
+        })
 
-      setTimeout(() => {
-        unsubscribeFn()
-        assert.deepEqual(observeArgs, [0, 2, 4, 6])
-        done()
-      }, 45)
+        setTimeout(() => {
+          unsubscribeFn()
+          assert.deepEqual(observeArgs, [0, 1, 3, 6])
+          done()
+        }, 45)
+      })
     })
-  })
 
-  describe('#filter()', function() {
-    it('should call observeFn with filtered args', function(done) {
-      let stream$ = new Stream( ({emit}) => {
-        let x = 0
-        let interval = setInterval(() => emit(x++), 10)
-        return () => clearInterval(interval)
+    describe('two subscriptions for one stream', () => {
+      it('should work independently', (done) => {
+        let s$1 = new Stream(({emit}) => {
+          let x = 0
+          let interval = setInterval(() => emit(x++), 10)
+          return () => clearInterval(interval)
+        })
+
+        let observeArgs1 = []
+        let observeArgs2 = []
+
+        let uns1 = s$1.observe((arg) => observeArgs1.push(arg))
+        let uns2
+        setTimeout(() => {
+          uns2 = s$1.observe((arg) => observeArgs2.push(arg))
+        }, 30)
+
+        setTimeout(() => {
+          uns1()
+          uns2()
+          assert.deepEqual(observeArgs1, [0, 1, 2, 3, 4])
+          assert.deepEqual(observeArgs2, [0, 1])
+          done()
+        }, 55)
       })
-      let filteredStream$ = stream$.filter((x) => x % 2 == 0)
-      let observeArgs = []
-      let unsubscribeFn = filteredStream$.observe((arg) => {
-        observeArgs.push(arg)
-      })
-
-      setTimeout(() => {
-        unsubscribeFn()
-        assert.deepEqual(observeArgs, [0, 2])
-        done()
-      }, 45)
-    })
-  })
-
-  describe('#scan()', function() {
-    it('should call observeFn with accumulator', function(done) {
-      let stream$ = new Stream(({emit}) => {
-        let x = 0
-        let interval = setInterval(() => emit(x++), 10)
-        return () => clearInterval(interval)
-      })
-      let scanedStream$ = stream$.scan(0, (x, y) => x + y)
-      let observeArgs = []
-      let unsubscribeFn = scanedStream$.observe((arg) => {
-        observeArgs.push(arg)
-      })
-
-      setTimeout(() => {
-        unsubscribeFn()
-        assert.deepEqual(observeArgs, [0, 1, 3, 6])
-        done()
-      }, 45)
-    })
-  })
-
-  describe('two subscribtions for one stream', function() {
-    it('one should work with delay', function(done) {
-      let s$1 = new Stream(({emit}) => {
-        let x = 0
-        let interval = setInterval(() => emit(x++), 10)
-        return () => clearInterval(interval)
-      })
-
-      let observeArgs1 = []
-      let observeArgs2 = []
-
-      let uns1 = s$1.observe((arg) => observeArgs1.push(arg))
-      let uns2
-      setTimeout(() => {
-        uns2 = s$1.observe((arg) => observeArgs2.push(arg))
-      }, 30)
-
-      setTimeout(() => {
-        uns1()
-        uns2()
-        assert.deepEqual(observeArgs1, [0, 1, 2, 3, 4])
-        assert.deepEqual(observeArgs2, [0, 1])
-        done()
-      }, 55)
     })
   })
 })
